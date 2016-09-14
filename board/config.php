b74ff14affc70ce9e50cba0d5e5a59fd52dc0714
Isaya Opondo <opondo.isaiah@gmail.com>
2016-09-14 10:27:33 +0300

    Update config.php

--- board/config.php
+++ board/config.php
@@ -1,8 +1,8 @@
 <?php
 	
 	define('SYSTEM_NAME','iCPDKenya');
-	define('WEBPHOTOROOT', '/home/icpduser/public_html/images/portraits/');
-	define('ACTIVITIESROOT', '/home/icpduser/public_html/images/activities/');
+	define('WEBPHOTOROOT', '/home/images/portraits/');
+	define('ACTIVITIESROOT', '/home/images/activities/');
 	//define('WEBPHOTOROOT', 'http://www.icpdkenya.org/images/portraits/');
 	//define('WEBPHOTOROOT', 'D:/xampp/htdocs/cpd/images/portraits/'); //local
 	define('WEBLINK','http://www.icpdkenya.org/'); // Website link
@@ -12,20 +12,20 @@
 	//$IMAGESIZE = 
 	define('MAX_SIZE',4000);
 	
-	define('SMTPUSER', 'synergy@medsynergy.info'); // sec. smtp username
-	define('SMTPPWD', 'Synergy2012##'); // sec. password
-	define('SMTPSERVER', 'mail.medsynergy.co.ke'); // sec. smtp server
+	define('SMTPUSER', ''); // sec. smtp username
+	define('SMTPPWD', ''); // sec. password
+	define('SMTPSERVER', ''); // sec. smtp server
 	
 
 	define('MailSupport','support@icpdkenya.org');
 	define('NOREPLY','noreply@icpdkenya.org');
 	
-/*	define('DBNAME', 'synergycpd');
+	define('DBNAME', 'synergycpd');
 	define('DBUSERNAME', 'admin');
 	define('DBPASS', 'password');
 	define('DBHOST', 'localhost');
 	define('DBTYPE', 'mysql');
-*/	
+	
 	define('CLAIM_DELIMITER',' ');
 	define('AUTHENTICATIONTYPE','mysql');
 	
@@ -35,17 +35,13 @@
 	define('ACTIVITYPASS',65);
 	define('MAXQUESTIONS',5);	
 
-	define('DBNAME', 'synergycpd');
-	define('DBUSERNAME', 'synergyadmin');
-	define('DBPASS', '3MySQL.DBSynergy@@');
-	define('DBHOST', '97.74.82.245');
-	define('DBTYPE', 'mysql');
+
 	
 	
 	function get_separator()	
 	{
 		$string = '|';
-		$salt = sha1('Synergy2010@@19811234'); 
+		$salt = sha1('Synergy2010##19811234'); 
         $salt = substr($salt, 0, 4); 
         $hash = base64_encode( $string . $salt ); 
 		
